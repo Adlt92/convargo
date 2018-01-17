324a05694fad9a72e74e818c58f437e17c144220
AugustinDlt <augustininscription@gmail.com>
2018-01-17 16:45:18 +0100

    step1

--- public/index.js
+++ public/index.js
@@ -7,12 +7,12 @@ var truckers = [{
   'name': 'les-routiers-bretons',
   'pricePerKm': 0.05,
   'pricePerVolume': 5
-}, {
+  }, {
   'id': '165d65ec-5e3f-488e-b371-d56ee100aa58',
   'name': 'geodis',
   'pricePerKm': 0.1,
   'pricePerVolume': 8.5
-}, {
+  }, {
   'id': '6e06c9c0-4ab0-4d66-8325-c5fa60187cf8',
   'name': 'xpo',
   'pricePerKm': 0.10,
@@ -39,7 +39,7 @@ var deliveries = [{
     'treasury': 0,
     'convargo': 0
   }
-}, {
+  }, {
   'id': '65203b0a-a864-4dea-81e2-e389515752a8',
   'shipper': 'librairie-lu-cie',
   'truckerId': '165d65ec-5e3f-488e-b371-d56ee100aa58',
@@ -54,7 +54,7 @@ var deliveries = [{
     'treasury': 0,
     'convargo': 0
   }
-}, {
+  }, {
   'id': '94dab739-bd93-44c0-9be1-52dd07baa9f6',
   'shipper': 'otacos',
   'truckerId': '6e06c9c0-4ab0-4d66-8325-c5fa60187cf8',
@@ -96,7 +96,7 @@ const actors = [{
     'type': 'credit',
     'amount': 0
   }]
-}, {
+  }, {
   'deliveryId': '65203b0a-a864-4dea-81e2-e389515752a8',
   'payment': [{
     'who': 'shipper',
@@ -119,7 +119,7 @@ const actors = [{
     'type': 'credit',
     'amount': 0
   }]
-}, {
+  }, {
   'deliveryId': '94dab739-bd93-44c0-9be1-52dd07baa9f6',
   'payment': [{
     'who': 'shipper',
@@ -144,6 +144,47 @@ const actors = [{
   }]
 }];
 
-console.log(truckers);
-console.log(deliveries);
-console.log(actors);
+function PrintConsole(element){
+  console.log(element);
+}
+
+function CommissionPrice(element){
+  element.commission.insurance = (element.price/2).toFixed(2);
+  element.commission.treasury = element.distance / 500;
+  element.commission.convargo = element.price - element.commission.insurance - element.commission.treasury;
+}
+
+function DiscountEvaluation(volume){
+  var decrease = 0;
+  if(volume >5){
+    decrease = 10;
+    if(volume >10){
+       decrease = 30;
+      if(volume >25){
+        decrease  =50;
+      }
+    }
+  }
+  return decrease;
+}
+
+function ShippingPrice()
+{
+  var discount;
+  var priceVolume;
+  deliveries.forEach(function(deli) {
+    truckers.forEach(function(truck) {
+      if(truck.id == deli.truckerId){
+        discount = DiscountEvaluation(deli.volume);
+        priceVolume = deli.volume * truck.pricePerVolume;
+        deli.price  = (priceVolume - (priceVolume * discount)/100 ) + ( deli.distance * truck.pricePerKm);
+      }
+    });
+    PrintConsole(deli);
+    //step 3
+    CommissionPrice(deli);
+
+  });
+}
+
+ShippingPrice();
